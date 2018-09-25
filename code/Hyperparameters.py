9b0e236afcf7d820cc25e6ce0cb514aa8c4603bb
Madhav Khosla <madhav.khoslaa@gmail.com>
2018-09-25 11:47:34 +0530

    Create Hyperparameters.py

---
+++ code/Hyperparameters.py
@@ -0,0 +1,6 @@
+class Hyperparams():
+    def __init__(self, learning_rate, batch_size, num_workers, n_iters, epoch):
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.n_iters = n_iters
